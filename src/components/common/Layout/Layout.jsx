66378619bae704b5322a03f2085b75003ae7c957
Miguel Garavito <mgaravitocamargo@gmail.com>
2023-06-24 01:20:36 -0500

    fix-pokedexImg

--- src/components/common/Layout/Layout.jsx
+++ src/components/common/Layout/Layout.jsx
@@ -1,8 +1,9 @@
 import { Outlet, useNavigate } from "react-router-dom"
-import "./Layout.css"
 import { UserNameContext } from "../../../context/UserNameContext";
 import { useContext } from "react";
-import home from "../../../assets/images/home.svg"
+import home from "../../../assets/images/home.svg";
+import pokedexImg from "../../../assets/images/pokedex.png";
+import "./Layout.css";
 
 const Layout = () => {
   const { removeUserName } = useContext(UserNameContext);
@@ -22,7 +23,7 @@ const Layout = () => {
             <div className="header-bottom"></div>
             <div className="header-pokedex">
               <img
-                src="https://s3-alpha-sig.figma.com/img/ca59/d9ce/98042af437fdff212d3259040db2e2db?Expires=1685923200&Signature=nMY1KszWu1~qq7kxGEJRphghbXXvBvk33QotdeoaHfrIMQ8SeDK~5lLdVz-3ujOHlZooSwxX8YPNCciZ3nX8lvIU4WCSLeeZj-h-KAYc~Ne33A3-rjiyRaMfRDzeLJ0XRN6YrkgNiiPHMb-Yu-p0d0h7nr1X7dBwUJKgy943Z~LUuGWS4tV7OkNz4Cf7BrIt2SVqPH00e8rTh44igGTaalgTSgKZU9XFe~DprWjxWc7owZcOYhJO9l88xicwoCjlAz4RytbcQhgBzrAUsBce0VkmAsH3q0XRDTjWCA5t7ed95QJUh0kw9QmQEogeQilFaiUxqVJtfk9VJC4cNYzJ9Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                src={pokedexImg}
                 alt="Pokedex"
               />
             </div>
